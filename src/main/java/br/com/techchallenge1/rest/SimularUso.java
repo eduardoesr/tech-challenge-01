45ad9068f1e52ba7e2f9e3bf33bd826e20d03a10
Eduardo Rodrigues <eduardoesr@live.com>
2024-11-04 18:03:59 -0300

    SimularUso

--- src/main/java/br/com/techchallenge1/rest/SimularUso.java
+++ src/main/java/br/com/techchallenge1/rest/SimularUso.java
@@ -14,6 +14,7 @@ import src.main.java.br.com.techchallenge1.model.ParceiroExterno;
 public class SimularUso {
     public static HashMap<String, ClasseAtivo> hashMapClasseAtivoPorNomeClasse = new HashMap<>(); // Obter classe pelo nome da classe
     public static HashMap<String, ClasseAtivo> HashMapClasseAtivoPorCodigoAtivo = new HashMap<>(); // Obter classe pelo código do ativo
+    public static HashMap<String, Ativo> HashMapAtivoPorCodigoAtivo = new HashMap<>(); // Obter ativo pelo código do ativo
     
     public static void main(String[] args) {
         configuracoesAdministrador();
@@ -43,13 +44,6 @@ public class SimularUso {
         classeAtivoInvestidorCripto.adicionarCategoriaAtivoInvestidor(categoriaAtivoB);
         classeAtivoInvestidorRendaFixa.adicionarCategoriaAtivoInvestidor(categoriaAtivoC);
         classeAtivoInvestidorBolsa.adicionarCategoriaAtivoInvestidor(categoriaAtivoD);
-
-        // Usuário: Ativo -> Preencher ativos -> MUDANÇA: Primeiro preencheu classe da categoria, depois os ativos
-        // DÚVIDA: Ativo precisa ser pré definido no sistema? O cliente só seleciona? Porque o sistema precisa de alguma forma ver o valor do ativo.
-        Ativo bitcoinAtivo = new Ativo("Bitcoin", "BTC");
-        Ativo ethereumAtivo = new Ativo("Ethereum",  "ETH");
-        Ativo nftAtivo = new Ativo("CapivaraDeChapeu", "CDC");
-        Ativo selicAtivo = new Ativo("Tesouro Selic 2027",  "BRSTNCLF1RL5");
 
         // AporteAtivoInvestidor aporteAtivoInvestidorBitcoin = new AporteAtivoInvestidor();
         // AtivoInvestidor bitcoinAtivoInvestidor = new AtivoInvestidor();
@@ -108,5 +102,17 @@ public class SimularUso {
         HashMapClasseAtivoPorCodigoAtivo.put("ETH", ativosCripto);
         HashMapClasseAtivoPorCodigoAtivo.put("CDC", ativosCripto);
         HashMapClasseAtivoPorCodigoAtivo.put("BRSTNCLF1RL5", ativosRendaFixa);
+
+        // Usuário: Ativo -> Preencher ativos -> MUDANÇA: Primeiro preencheu classe da categoria, depois os ativos
+        // DÚVIDA: Ativo precisa ser pré definido no sistema? O cliente só seleciona? Porque o sistema precisa de alguma forma ver o valor do ativo.
+        Ativo bitcoinAtivo = new Ativo("Bitcoin", "BTC");
+        Ativo ethereumAtivo = new Ativo("Ethereum",  "ETH");
+        Ativo nftAtivo = new Ativo("CapivaraDeChapeu", "CDC");
+        Ativo selicAtivo = new Ativo("Tesouro Selic 2027",  "BRSTNCLF1RL5");
+
+        HashMapAtivoPorCodigoAtivo.put("BTC", bitcoinAtivo);
+        HashMapAtivoPorCodigoAtivo.put("ETH", ethereumAtivo);
+        HashMapAtivoPorCodigoAtivo.put("CDC", nftAtivo);
+        HashMapAtivoPorCodigoAtivo.put("BRSTNCLF1RL5", selicAtivo);
     }
 }
